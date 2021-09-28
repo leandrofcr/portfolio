4e2c12bf6b4b85c779e6ddd8f2c2b2db2953b20b
Leandro Reis <lleandrofr@gmail.com>
2021-09-28 15:39:29 -0300

    improvement: add cookmaster-api project

--- src/content/projects.js
+++ src/content/projects.js
@@ -1,19 +1,24 @@
-import { amongUs, onlineShop, triviaGame } from '../images';
+import {
+  triviaGame,
+  cookmaster,
+  onlineShop,
+  amongUs,
+} from '../images';
 
 const projects = [
-  {
-    name: 'Among Us Timer',
-    description: 'This project is a themed timer based on the Among us game, developed with React and styled to bring a similar immersion to the game. ',
-    image: amongUs,
-    url: 'https://leandrofcr.github.io/among-us-timer/',
-    tags: ['React', 'JavaScript', 'CSS3'],
-  },
   {
     name: 'Trivia Game',
     description: 'The Trivia Game project is a quiz game based on a quiz game using React and Redux, developed its functionalities according to the demands defined in a Kanban board.',
     image: triviaGame,
     url: 'https://leandrofcr.github.io/trivia-game/',
     tags: ['React', 'Redux', 'JavaScript', 'CSS3', 'react-spring'],
+  },
+  {
+    name: 'Cookmaster API',
+    description: 'Cookmaster is a API for registration and search of recipes, using JWT, Multer, MSC and REST architecture.',
+    image: cookmaster,
+    url: 'https://github.com/leandrofcr/cookmaster-api',
+    tags: ['Node.js', 'API REST', 'JWT', 'Express.js', 'MSC'],
   },
 
   {
@@ -22,6 +27,13 @@ const projects = [
     image: onlineShop,
     url: 'https://github.com/leandrofcr/online-store',
     tags: ['React', 'JavaScript', 'MercadoLivre API'],
+  },
+  {
+    name: 'Among Us Timer',
+    description: 'This project is a themed timer based on the Among us game, developed with React and styled to bring a similar immersion to the game. ',
+    image: amongUs,
+    url: 'https://leandrofcr.github.io/among-us-timer/',
+    tags: ['React', 'JavaScript', 'CSS3'],
   },
 ];
 
--- src/images/index.js
+++ src/images/index.js
@@ -2,3 +2,4 @@ export { default as profilePic } from './me.png';
 export { default as amongUs } from './amongUsTimer.png';
 export { default as onlineShop } from './onlineShop.png';
 export { default as triviaGame } from './triviaGame.png';
+export { default as cookmaster } from './cookmaster.png';
